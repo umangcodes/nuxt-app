cda324a199835a9dfc080dbd23b670ae7cd58f32
Umang A <umanga.eng@gmail.com>
2021-08-13 18:48:29 -0400

    links added

--- components/header.vue
+++ components/header.vue
@@ -1,10 +1,19 @@
 <template>
-<div>
+<div class="nav">
+    <div v-for="nav in navLinks" :key="nav.id">
+    <nuxt-link :to="nav.url" class="brand">{{nav.title}}</nuxt-link>
+</div>
 </div>
 </template>
 
 <script>
     export default {
+        data(){
+            return{
+                navLinks:[{id: 91, title:"Home", url:"/"},
+                {id: 92, title:"Jokes", url:"/jokes"},]
+            }
+        }
         
     }
 </script>
