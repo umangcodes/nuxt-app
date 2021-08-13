ceba8857ae750fdec3d13210780dafc286b99282
Umang A <umanga.eng@gmail.com>
2021-08-13 18:49:36 -0400

    styles added

--- components/header.vue
+++ components/header.vue
@@ -18,6 +18,32 @@
     }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+.brand {
+      font-family: 'Montserrat', sans-serif;
+      font-weight: 700;
+      font-size: 1.5em;
+      color: #39b982;
+      text-decoration: none;
+    }
+    .nav {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      height: 60px;
+    }
+    .nav .nav-item {
+      box-sizing: border-box;
+      margin: 0 5px;
+      color: rgba(0, 0, 0, 0.5);
+      text-decoration: none;
+    }
+    .nav .nav-item.router-link-exact-active {
+      color: #39b982;
+      border-bottom: solid 2px #39b982;
+    }
+    .nav a {
+      display: inline-block;
+    }
 
 </style>
