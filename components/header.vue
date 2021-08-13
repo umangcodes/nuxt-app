364b4732adc8ee40cca8091ebdf7a6846ee663b9
Umang A <umanga.eng@gmail.com>
2021-08-13 19:15:19 -0400

    styles added and nav bar modified

--- components/header.vue
+++ components/header.vue
@@ -1,49 +1,50 @@
 <template>
-<div class="nav">
-    <div v-for="nav in navLinks" :key="nav.id">
-    <nuxt-link :to="nav.url" class="brand">{{nav.title}}</nuxt-link>
-</div>
-</div>
+  <div class="nav">
+    <nuxt-link to="/" class="brand">Jokes App</nuxt-link>
+    <nav v-for="nav in navLinks" :key="nav.id">
+      <nuxt-link :to="nav.url">{{ nav.title }}</nuxt-link>
+    </nav>
+  </div>
 </template>
 
 <script>
-    export default {
-        data(){
-            return{
-                navLinks:[{id: 91, title:"Home", url:"/"},
-                {id: 92, title:"Jokes", url:"/jokes"},]
-            }
-        }
-        
+export default {
+  data() {
+    return {
+      navLinks: [
+        { id: 91, title: 'Home', url: '/' },
+        { id: 92, title: 'Jokes', url: '/jokes' },
+      ],
     }
+  },
+}
 </script>
 
 <style scoped>
 .brand {
-      font-family: 'Montserrat', sans-serif;
-      font-weight: 700;
-      font-size: 1.5em;
-      color: #39b982;
-      text-decoration: none;
-    }
-    .nav {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      height: 60px;
-    }
-    .nav .nav-item {
-      box-sizing: border-box;
-      margin: 0 5px;
-      color: rgba(0, 0, 0, 0.5);
-      text-decoration: none;
-    }
-    .nav .nav-item.router-link-exact-active {
-      color: #39b982;
-      border-bottom: solid 2px #39b982;
-    }
-    .nav a {
-      display: inline-block;
-    }
-
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 700;
+  font-size: 1.5em;
+  color: #39b982;
+  text-decoration: none;
+}
+.nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 60px;
+}
+.nav .nav-item {
+  box-sizing: border-box;
+  margin: 0 5px;
+  color: rgba(0, 0, 0, 0.5);
+  text-decoration: none;
+}
+.nav .nav-item.router-link-exact-active {
+  color: #39b982;
+  border-bottom: solid 2px #39b982;
+}
+.nav a {
+  display: inline-block;
+}
 </style>
