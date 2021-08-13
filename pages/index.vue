db14e981bce32d319d84766e9d4f74a31cf8ba79
Umang A <umanga.eng@gmail.com>
2021-08-13 19:46:43 -0400

    seo tags added

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <h1>Home Page
-    </h1>
+    <h1>Home Page</h1>
   </div>
 </template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Home',
+    }
+  },
+}
+</script>
--- pages/jokes.vue
+++ pages/jokes.vue
@@ -1,5 +1,23 @@
 <template>
-    <div>
-        <h1>Jokes page</h1>
-    </div>
+  <div>
+    <h1>Jokes page</h1>
+  </div>
 </template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Jokes',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            'use the link to search for jokes with criteria. Powered by jokeApi v2',
+        },
+      ],
+    }
+  },
+}
+</script>
