f3c7727fe111641e9820a1e4add0182c9cd8097c
Umang A <umanga.eng@gmail.com>
2021-08-14 03:20:13 -0400

    nested search page created

---
+++ pages/search/_joke.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <span>This is joke : {{ id }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Joke #' + this.id,
+      meta: [
+        {
+          hid: 'joke' + this.id,
+          name: 'dazzle',
+          description: `individual joke number ${this.id}`,
+        },
+      ],
+    }
+  },
+  computed: {
+    id() {
+      return this.$route.params.joke
+    },
+  },
+}
+</script>
+
+<style></style>
