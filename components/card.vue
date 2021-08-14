d984a8f6d4823a46bee2e71a32bf1bcface8fb91
Umang A <umanga.eng@gmail.com>
2021-08-14 02:48:05 -0400

    card comp created, tested

---
+++ components/card.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="min-h-screen bg-gray-100 grid items-center justify-center">
+    <div
+      class="
+        p-6
+        bg-white
+        flex
+        items-center
+        space-x-6
+        rounded-lg
+        shadow-md
+        hover:scale-105
+        transition
+        transform
+        duration-500
+        cursor-pointer
+      "
+    >
+      <div>
+        <h1 class="text-xl font-bold text-gray-700 mb-2">
+          {{ data.category }} {{ data.id }}
+        </h1>
+        <p class="text-gray-600 w-80 text-sm">
+          {{ data.joke }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      required: true,
+      type: Object,
+    },
+  },
+}
+</script>
+
+<style scoped></style>
